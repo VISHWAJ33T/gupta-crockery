afff3944806ef9f6c798c233c5b7a510e6a20d3e
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-08-22 20:12:05 +0530

    enabled purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: false, // Disable PurgeCSS entirely
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+      './app/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
   theme: {
     extend: {
       screens: {
-        "3xl": "1600px",
-        "xs": "475px",
+        '3xl': '1600px',
+        'xs': '475px',
       },
     },
   },
