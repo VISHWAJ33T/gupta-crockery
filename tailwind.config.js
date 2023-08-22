206bfb3183b622db856c464a916bf7bbc7b55d44
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-08-22 20:17:23 +0530

    added some safelist classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === "production",
     content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-      './app/**/*.{js,ts,jsx,tsx}',
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./app/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      "sticky",
+      "w-full",
+      "top-0",
+      "z-[100]",
+      "justify-between",
+      "space-x-1",
+      "ml-4",
+      "mr-3",
     ],
   },
   theme: {
     extend: {
       screens: {
-        '3xl': '1600px',
-        'xs': '475px',
+        "3xl": "1600px",
+        xs: "475px",
       },
     },
   },
