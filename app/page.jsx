503a37bee1c6a191f32cbe76df6fefe481557a59
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-08-25 03:53:45 +0530

    fixed font

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import CarouselContainer from "@/Components/Carousel";
 import Categories from "@/Components/Categories";
 import CategoryContainer from "@/Components/CategoryContainer";
@@ -6,9 +5,6 @@ export default function Home() {
 
   return (
     <>
-      <Head><link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet" /></Head>
       <div className="w-[100vw]">
         {/* <div className="h-[25vh] sm:h-[50vh] xl:h-[55vh] w-[100vw]"> */}
         <CarouselContainer />
