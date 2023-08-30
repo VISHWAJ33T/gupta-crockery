53607502a3f176ed7b20b4cf9d522d2115d65b21
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-08-30 16:09:58 +0530

    minor changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,10 +2,9 @@ import { Analytics } from '@vercel/analytics/react';
 import Footer from "@/Components/Footer";
 import Nav from "@/Components/Nav";
 import "@/styles/globals.css";
-
 export const metadata = {
   title: "Gupta Crockery",
-  description: "Gupta Crockery Description",
+  description: `Introducing "Gupta Crockery" – Where Quality Meets Savings! Explore stunning crockery that's kind to your wallet. Find beauty and trust in every piece, carefully selected to bring elegance to your table without the hefty price tag. Enjoy luxury for less at Gupta Crockery!`,
 };
 
 export default function RootLayout({ children }) {
