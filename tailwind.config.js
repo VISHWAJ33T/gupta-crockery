2ac3dd71c60361631b8080d345be1f68d22b1d2d
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-09-06 20:00:58 +0530

    added more loading items

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    ".//**/*.{js,ts,jsx,tsx}",
+    // ".//**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
