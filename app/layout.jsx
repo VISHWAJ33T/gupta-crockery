df116edb46468c00b9de03e3c235d1e0e4f5745a
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-09-07 18:28:43 +0530

    PWA fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,17 +4,27 @@ import Nav from "@/Components/Nav";
 import "@/styles/globals.css";
 export const metadata = {
   title: "Gupta Crockery",
-  description: `Introducing "Gupta Crockery" – Where Quality Meets Savings! Explore stunning crockery that's kind to your wallet. Find beauty and trust in every piece, carefully selected to bring elegance to your table without the hefty price tag. Enjoy luxury for less at Gupta Crockery!`,
+  description: `Introducing "Gupta Crockery" - Where Quality Meets Savings! Explore stunning crockery that's kind to your wallet. Find beauty and trust in every piece, carefully selected to bring elegance to your table without the hefty price tag. Enjoy luxury for less at Gupta Crockery!`,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+
+        <meta name="application-name" content="Gupta Crockery" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Ecommerce App" />
+        <meta name="description" content='Introducing "Gupta Crockery" - Where Quality Meets Savings! Explore stunning crockery thats kind to your wallet. Find beauty and trust in every piece, carefully selected to bring elegance to your table without the hefty price tag. Enjoy luxury for less at Gupta Crockery!' />
+        <meta name="format-detection" content="+91-9463916728" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="none" />
+        <meta name="msapplication-TileColor" content="#131b2e" />
+        <meta name="msapplication-tap-highlight" content="no" />
+
         <link rel="icon" href="/favicon.ico" />
-        {/* <!-- link manifest.json --> */}
         <link rel="manifest" href="/manifest.json" />
-        {/* <!-- this sets the color of url bar  --> */}
         <meta name="theme-color" content="#131b2e" />
       </head>
       <body>
