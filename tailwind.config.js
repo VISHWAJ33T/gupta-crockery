f5b3609b2351df2b8a84af75862052638fa67607
Your NameVishwajeet <vishwajeety14122@gmail.com>
2023-10-11 20:34:34 +0530

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
   content: [
     // ".//**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  mode: 'jit',
   theme: {
     extend: {
       screens: {
